72187a1e0fb8309f5a11addf7d2213ead39233c7
priess0 <honza.svejdik@gmail.com>
2022-07-27 01:22:19 +0200

    Completed 02 - with a little help from someone's solution - using "repeat" method Commented my attempted solution of "for" loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    return string.repeat(num);
+  }
 };
+
+
+/*
+const repeatString = function(string, num) {
+  let tries = num;
+  let result = "";
+  for (let num = 0; num < tries; num++) {
+      return result += string;
+    }
+};
+*/
+
 
 // Do not edit below this line
 module.exports = repeatString;
